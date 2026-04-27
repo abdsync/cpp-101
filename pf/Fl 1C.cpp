ae0bf243bb6fa1f0323c1f063ae0fcc52c240b94
abdsync <muhammed.abdulah21@gmail.com>
2026-04-28 00:43:48 +0500

    Removed folder from repository

--- pf/Fl 1C.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main ()
-{
-	for(int i=20; i>=2; i-=2)
-	{
-		cout<<i<<endl;
-	}
-}
--- pf/Libraries.cpp
+++
@@ -1,3 +0,0 @@
-#include <iostream> // for input output stream (cin, cout)
-#include <vector>   // for using the vector container 
-#include <cmath>    // for mathematical functions 
